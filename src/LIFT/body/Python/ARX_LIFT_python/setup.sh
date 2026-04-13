13384721b1a20d5c20a453e295baa1b2c895c7a7
pickxiguapi <1084612722@qq.com>
2026-04-13 14:46:01 +0800

    Replace LIFT submodule with xianzeYao/ARX_LIFT2 (latest main)

--- src/LIFT/body/Python/ARX_LIFT_python/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=$(pwd)/lift_controller/api/arx_lift_src:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$(pwd)/lift_controller/api/arx_hardware_interface:$LD_LIBRARY_PATH
